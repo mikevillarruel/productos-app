60ef62469e3ace0f978229518e38da47f794e23f
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-09 01:05:35 -0500

    initial setup

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,24 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { LoginScreen } from '../screens/LoginScreen';
+import { ProtectedScreen } from '../screens/ProtectedScreen';
+import { RegisterScreen } from '../screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: 'white',
+                }
+            }}
+        >
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} />
+        </Stack.Navigator>
+    )
+}
